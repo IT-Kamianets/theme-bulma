569824bde3464469fd0834a342928f732597ac0c
AnatoliyKN1B22 <kn1b22.humelnyk@kpnu.edu.ua>
2026-02-02 13:39:54 +0200

    feat: add Home and List pages with routing
    - Implemented routing for Home and List components in app.routes.ts
    - Created Home component with a welcoming hero section and advantages display
    - Added List component with a placeholder message
    - Styled Home component with custom CSS effects for cards
    - Added unit tests for Home and List components
    - Configured main.scss to include Bulma framework

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { List } from './pages/list/list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'list',
+        component: List,
+    },
+    
+];
+
